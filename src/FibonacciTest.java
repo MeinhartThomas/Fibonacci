a5dc972c653f88221e9a655877045379ff98ecd0
MeinhartThoma <thomas.meinhart@icloud.com>
2017-03-10 15:41:05 +0100

    Initial Commit

---
+++ src/FibonacciTest.java
@@ -0,0 +1,15 @@
+/**
+ * Created by phili_000 on 03.03.2017.
+ */
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FibonacciTest {
+        @Test
+        public void evaluatesExpression() {
+            Fibonacci fibonacci = new Fibonacci();
+            assertEquals(13, fibonacci.calculate(7));
+        }
+}
